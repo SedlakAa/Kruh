ed53a9e956cbcd5162ee4b8d587fc6a978c979b4
Adam Sedlák <Sedlak.Adam@stud.vassboskovice.cz>
2021-11-02 10:16:34 +0100

    Add project files.

---
+++ Kruh/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Kruh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
